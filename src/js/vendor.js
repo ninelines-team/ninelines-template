431f811793be855f666661791626489aeaeebde9
Dmitry Artemov <art.beliarh@gmail.com>
2017-08-04 10:47:16 +0400

    Добавил babel-polyfill

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,3 +1,4 @@
+// @include('../../node_modules/babel-polyfill/dist/polyfill.js')
 // @include('../../node_modules/bowser/bowser.js')
 // @include('../../node_modules/svg4everybody/dist/svg4everybody.js')
 // @include('../../node_modules/jquery/dist/jquery.js')
