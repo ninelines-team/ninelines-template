e1d88c5429f6e228d7fb07eb8475980dcbf458fe
Dmitry Artemov <art.beliarh@gmail.com>
2018-10-21 03:15:24 +0400

    Вынес конфигурацию babel в отдельный файл

--- webpack.config.js
+++ webpack.config.js
@@ -13,14 +13,6 @@ module.exports = {
 				exclude: /node_modules|bower_components/,
 				use: {
 					loader: 'babel-loader',
-					options: {
-						presets: [
-							'@babel/preset-env',
-						],
-						plugins: [
-							'@babel/plugin-transform-runtime',
-						],
-					},
 				},
 			},
 		],
