fef60faf686ff5713b45e3100ba0c1bb2e606644
vladislav <vladislav.051094@gmail.com>
2019-12-17 10:05:04 +0300

    removed unnecessary code from shareSettings.php

--- src/resources/shareSettings.php
+++ src/resources/shareSettings.php
@@ -1,18 +1,15 @@
 <?php
 
-$host = '';
 $title = '';
 $description = '';
 $image = '';
-$redirect = '/';
 
 // Uncomment the code below and fill in the pages if necessary
 // $pages = [
-// 	'page/1' => [
+// 	'/page/1' => [
 // 		'title' => '',
 // 		'description' => '',
 // 		'image' => '',
-// 		'redirect' => '',
 // 	],
 // ];
 
@@ -22,7 +19,6 @@ if ($page) {
 	$title = !is_null(@$page['title']) ? $page['title'] : $title;
 	$description = !is_null(@$page['description']) ? $page['description'] : $description;
 	$image = !is_null(@$page['image']) ? $page['image'] : $image;
-	$redirect = !is_null(@$page['redirect']) ? $page['redirect'] : $redirect;
 }
 
 ?>
