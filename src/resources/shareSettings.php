e4a12a7654bbf3e959e3418bd69e9efc08c3f279
DaveDev <deniskobeleff@yandex.ru>
2020-01-28 15:22:00 +0800

    sharing fixed: absolute path for the image

--- src/resources/shareSettings.php
+++ src/resources/shareSettings.php
@@ -1,15 +1,17 @@
 <?php
 
+$protocol = $_SERVER['PROTOCOL'] = isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) ? 'https' : 'http';
+$host = $protocol . '://' . $_SERVER['HTTP_HOST'];
 $title = '';
 $description = '';
-$image = '';
+$image = $host . '/images/';
 
 // Uncomment the code below and fill in the pages if necessary
 // $pages = [
 // 	'/page/1' => [
 // 		'title' => '',
 // 		'description' => '',
-// 		'image' => '',
+// 		'image' => '/images/',
 // 	],
 // ];
 
@@ -18,7 +20,7 @@ $page = @$pages[$_SERVER['REQUEST_URI']];
 if ($page) {
 	$title = !is_null(@$page['title']) ? $page['title'] : $title;
 	$description = !is_null(@$page['description']) ? $page['description'] : $description;
-	$image = !is_null(@$page['image']) ? $page['image'] : $image;
+	$image = !is_null(@$page['image']) ? $host . $page['image'] : $image;
 }
 
 ?>
