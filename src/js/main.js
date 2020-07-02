50ab7128d67a55c2dac61a8f443862e4ae1a3865
Ilya Konovalov <i.konovalov@admitad.pro>
2020-07-02 21:56:56 +0300

    first commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,7 @@
-import './vendor';
-import './helpers';
+import bulmaCarousel from '../../node_modules/bulma-carousel/dist/js/bulma-carousel.min.js';
+
+bulmaCarousel.attach('#carousel-demo', {
+	slidesToScroll: 1,
+	slidesToShow: 4,
+	pagination: false
+});
