5a67ec00cf6ee179115757c2914aa799dc06c6d0
v-ershov <sully.weber@gmail.com>
2021-06-15 19:00:04 +0300

    Fix Babel

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,4 +1,6 @@
-import '@babel/polyfill';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 import objectFitImages from 'object-fit-images';
